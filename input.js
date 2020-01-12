e555a7eb254022ad557598aaea01008318dc21d3
dakadabra <david.a.khazzam@gmail.com>
2020-01-12 02:52:12 -0500

    Made functions more concise

--- input.js
+++ input.js
@@ -1,9 +1,3 @@
-var linkedin_points;
-var github_points;
-var website_points;
-var workexp_points;
-var skill_points;
-
 function updateProgress() {
   move(linkedin(document.getElementById("linkedIn").value), "linkedInBar");
   move(github(document.getElementById("gitHub").value), "gitHubBar");
@@ -27,65 +21,35 @@ function move(amountMoved, barName) {
 }
 
 function linkedin(input){
-  if(String (input)==null || String (input).search("linkedin.com/in/")==-1){
-    linkedin_points =0;
-  }
-  else{
-    linkedin_points =100;
-  }
-  return linkedin_points;
+  if(String (input)==null || String (input).search("linkedin.com/in/")==-1) return 0
+  return 100
 }
 
 function github(input){
-  if(String(input)==null || String(input).search("github.com/")==-1){
-    github_points=0;
-  }
-  else{
-    github_points=100;
-  }
-  return github_points;
+  if(String(input)==null || String(input).search("github.com/")==-1) return 0
+  return 100
 }
 
 function website(input){
-  if(input=="") {
-    website_points =0;
-  }
-  else{
-    website_points =100;
-  }
-  return website_points;
+  if(input=="") return 0
+  return 100
 }
 
 function workexp(input){
   if (input == "") count = 0;
   else count = input.split(/\r\n|\r|\n/).length;
-  if(count==0){
-    return workexp_points=0;
-  }
-  else if(count==1){
-    return workexp_points=25;
-  }
-  else if(count==2){
-    return workexp_points=50;
-  }
-  else if(count>=3){
-    return workexp_points=100;
-  }
+
+  if (count==0) return 0
+  else if(count==1) return 25
+  else if(count==2) return 50
+  else if(count>=3) return 100
 }
 
 function skill(input){
   if (input == "") count = 0;
   else count = input.split(/\r\n|\r|\n/).length;
-  if(count==0){
-    return skill_points=0;
-  }
-  else if(0<count<=3){
-    return skill_points=25;
-  }
-  else if(3<count<=6){
-    return skill_points=50;
-  }
-  else if(count>6){
-    return skill_points=100;
-  }
+  if(count==0) return 0
+  else if(0<count<=3) return 25
+  else if(3<count<=6) return 50
+  else if(count>6) return 100
 }
